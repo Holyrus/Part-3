1ad9b04d610e256a52d02edbd4eda4322b82b029
Ruslan <rus.onypenko@gmail.com>
2024-12-06 00:33:30 +0200

    Completed 3.8

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 app.use(express.json());
 
+morgan.token('body', (request) => {
+    return JSON.stringify(request.body);
+})
+
+app.use(
+    morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
 app.get('/api/persons', (request, response) => {
     response.json(persons);
