faa9152e650ad43084f1638bb2eb186cc7657a24
Ruslan <rus.onypenko@gmail.com>
2024-12-12 23:43:35 +0200

    3.18* in process..

--- index.js
+++ index.js
@@ -32,23 +32,36 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person);
-    })
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-// app.get('/info', (request, response) => {
-//     let currentTime = new Date().toString();
-//     response.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${currentTime}</p>`)
-// })
+// Figuring out how many document in database
 
-// app.delete('/api/persons/:id', (request, response) => {
-//     const id = request.params.id;
-//     persons = persons.filter(pers => pers.id !== id);
+const uri = process.env.MONGODB_URI;
 
-//     response.status(204).end();
-// })
+app.get('/info', (request, response) => {
+    let currentTime = new Date().toString();
+    response.send(`<p>Phonebook has info ? for people</p> <p>${currentTime}</p>`)
+})
+
+// ------------------------------------------
+
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
 
 app.post('/api/persons', (request, response) => {
     const body = request.body;
@@ -72,6 +85,33 @@ app.post('/api/persons', (request, response) => {
         response.json(savedPerson)
     })
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body;
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'Malformatted ID'})
+    }
+
+    next(error)
+}
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
