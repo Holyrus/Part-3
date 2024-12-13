0b2bf473f4d666116073889123f04dcec17820c2
Veronika Pozdniakova <800053068@omnia.fi>
2024-12-13 18:23:52 +0200

    Working on 3.19*

--- models/person.js
+++ models/person.js
@@ -15,8 +15,22 @@ mongoose.connect(url)
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: Number,
+    name: {
+        type: String,
+        minLength: 3,
+        required: true 
+    },
+    number: {
+        type: String,
+        validate: {
+            validator: function(v) {
+                return /^\d{2,3}-\d{5,10}$/.test(v);
+            },
+            message: props => `${props.value} is note a valid phone number!`
+        },
+        minLength: 8,
+        required: true
+    }
 })
 
 personSchema.set('toJSON', {
